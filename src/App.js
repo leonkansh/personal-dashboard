1b422fb83ae2682bc518b9f0bb6b59a389988b1e
Leon Kan <leonkan.shcn@gmail.com>
2020-08-27 23:35:00 -0700

    display covid19 info

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import './App.css';
 import Weather from "./Weather";
 import News from "./News";
 import ToDo from "./ToDo";
+// import Covid from "./Covid";
+import Covid2 from "./Covid2";
+
 
 function App() {
   return (
-  <main>
-    <h1>Greetings, Leon</h1>
-    <Weather/>
-    <section>
-      <div>
-        <h3>Latest headlines</h3>
-        <News/>
-      </div>
-      <div>
-        <h3>Today's Agenda</h3>
-        <ToDo/>
-      </div>
-    </section>
-  </main>
+    <main>
+      <h1>Greetings, Leon</h1>
+      <Weather />
+      <section>
+        <div>
+          <h3>Latest headlines</h3>
+          <News />
+        </div>
+        <div>
+          <h3>Today's Agenda</h3>
+          <ToDo />
+        </div>
+        <div id="cases">
+          <h3>Cases in the US</h3>
+          <p id="root"></p>
+          {/* <Covid/> */}
+          <Covid2 />
+        </div>
+      </section>
+    </main>
   );
 }
 
